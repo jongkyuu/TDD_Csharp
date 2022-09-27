41301a8cfb9e0f54358233f74e38f68b8d839404
이종규 <jongkyu.lee@mirero.co.kr>
2022-09-27 14:12:56 +0900

    MSTest 사용한 unit test

---
+++ unit-testing-using-mstest/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimeService;
+
+namespace PrimeService.Tests
+{
+    [TestClass]
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [TestMethod]
+        public void isPrime_InputIs1_ReturnFalse()
+        {
+            bool result = _primeService.IsPrime(1);
+
+            Assert.IsFalse(result, "1 sould not be prime");
+        }
+
+        [TestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        public void isPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
+    }
+}
