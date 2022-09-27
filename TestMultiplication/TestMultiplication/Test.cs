29e4a00075f75e5144aee6193d759a8037a648ce
minerva421 <minerva421.ai@gmail.com>
2022-09-28 01:48:56 +0900

    TDD - ch3

--- TestMultiplication/TestMultiplication/Test.cs
+++ TestMultiplication/TestMultiplication/Test.cs
@@ -12,6 +12,13 @@
             product = five.Times(3);
             Assert.AreEqual(15, product.amount);
         }
+
+        [TestMethod]
+        public void TestEquality()
+        {
+            Assert.IsTrue(new Dollar(5).equals(new Dollar(5)));
+            Assert.IsFalse(new Dollar(5).equals(new Dollar(6)));
+        }
     }
 
     class Dollar
@@ -26,6 +33,12 @@
         public Dollar Times(int multiplier)
         {
             return new Dollar(amount * multiplier);
+        }
+
+        public bool equals(object @object)
+        {
+            Dollar dollar = (Dollar)@object;
+            return amount == dollar.amount;
         }
     }
 }
