1c23f0da460aee8f62c25cedc069a77ad64e4071
이종규 <jongkyu.lee@mirero.co.kr>
2022-09-27 17:30:57 +0900

    TDD - ch1

---
+++ TestMultiplication/TestMultiplication/Test.cs
@@ -0,0 +1,29 @@
+﻿namespace TestMultiplication
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            Dollor five = new Dollor(5);
+            five.Times(2);
+            Assert.AreEqual(10, five.amount);
+        }
+    }
+
+    class Dollor
+    {
+        public int amount;
+
+        public Dollor(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public void Times(int multiplier)
+        {
+            amount *= multiplier;
+        }
+    }
+}
