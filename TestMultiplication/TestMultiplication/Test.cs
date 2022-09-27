f7817a019355e826b40d7759256398679a9503ee
minerva421 <minerva421.ai@gmail.com>
2022-09-28 02:18:53 +0900

    TDD - ch5

--- TestMultiplication/TestMultiplication/Test.cs
+++ TestMultiplication/TestMultiplication/Test.cs
@@ -17,6 +17,14 @@
             Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
             Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
         }
+
+        [TestMethod]
+        public void TestFrancMultiplication()
+        {
+            Franc five = new Franc(5);
+            Assert.AreEqual(new Franc(10), five.Times(2));
+            Assert.AreEqual(new Franc(15), five.Times(3));
+        }
     }
 
     class Dollar
@@ -43,6 +51,35 @@
             {
                 Dollar dollar = (Dollar)obj;
                 return amount == dollar.amount;
+            }
+        }
+    }
+
+
+    class Franc
+    {
+        private int amount;
+
+        public Franc(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public Franc Times(int multiplier)
+        {
+            return new Franc(amount * multiplier);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Franc franc = (Franc)obj;
+                return amount == franc.amount;
             }
         }
     }
