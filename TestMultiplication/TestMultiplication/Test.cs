16bde4a42e4622ca15bf84885b7252663ab4e5ac
minerva421 <minerva421.ai@gmail.com>
2022-10-03 02:50:49 +0900

    TDD - ch7

--- TestMultiplication/TestMultiplication/Test.cs
+++ TestMultiplication/TestMultiplication/Test.cs
@@ -18,15 +18,16 @@
             Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
             Assert.IsTrue(new Franc(5).Equals(new Franc(5)));
             Assert.IsFalse(new Franc(5).Equals(new Franc(6)));
+            Assert.IsFalse(new Franc(5).Equals(new Dollar(5)));
         }
 
-        [TestMethod]
-        public void TestFrancMultiplication()
-        {
-            Franc five = new Franc(5);
-            Assert.AreEqual(new Franc(10), five.Times(2));
-            Assert.AreEqual(new Franc(15), five.Times(3));
-        }
+        //[TestMethod]
+        //public void TestFrancMultiplication()
+        //{
+        //    Franc five = new Franc(5);
+        //    Assert.AreEqual(new Franc(10), five.Times(2));
+        //    Assert.AreEqual(new Franc(15), five.Times(3));
+        //}
     }
 
     class Money
@@ -41,7 +42,7 @@
             //}
 
             Money money = (Money)obj;
-            return amount == money.amount;
+            return amount == money.amount && GetType().Equals(money.GetType());
         }
     }
 
